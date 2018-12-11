09a867774a2fdd10e77c619c9f5db9216efca870
zzzfb <31818131+zzzfb@users.noreply.github.com>
2018-12-11 20:39:31 +0800

    动态规划

--- src/礼物的最大值/MaxValueOfGiftTest.java
+++ src/礼物的最大值/MaxValueOfGiftTest.java
@@ -6,6 +6,7 @@ public class MaxValueOfGiftTest {
         //int[] a={1,10,3,8,12,2,9,6,5,7,4,11,3,7,16,5};
         //int[] a={1};
         int[] a={1,2,3};
+        
 
         MaxValueOfGift mvg=new MaxValueOfGift();
         System.out.println(mvg.getMaxValue(a,3,1));
