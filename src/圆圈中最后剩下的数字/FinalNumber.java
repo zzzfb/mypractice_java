f52036607d07dc4f305241e284f2749e60091d75
ZFB <316128660@qq.com>
2018-12-26 21:41:20 +0800

    2018/12/26 两种解法

--- src/圆圈中最后剩下的数字/FinalNumber.java
+++ src/圆圈中最后剩下的数字/FinalNumber.java
@@ -26,6 +26,11 @@ public class FinalNumber {
         }
     }
 
+    public void remove(Node node){
+        node.val=node.next.val;
+        node.next=node.next.next;
+    }
+
     public int getFinalNumber(int n,int m){
         if(n<=0||m<=0){
             return -1;
@@ -34,10 +39,23 @@ public class FinalNumber {
             this.add(i);
         }
         Node node=head;
-        while (head.next!=head){
-            for (int i=0;i<m;i++){
+        while (node.next!=node){  //不应用head.next！=head判断
+            for (int i=0;i<m-1;i++){ //m-1，当前开始数，本身算一个
                 node=node.next;
             }
+            remove(node);
         }
+        return node.val;
+    }
+
+    public int getFinalNumberQuick(int n,int m){  //创新解法，利用映射
+        if(n<=0||m<=0){
+            return -1;
+        }
+        int last=0;
+        for (int i=2;i<=n;i++){
+            last=(last+m)%i;
+        }
+        return last;
     }
 }
---
+++ src/圆圈中最后剩下的数字/FinalNumberTest.java
@@ -0,0 +1,9 @@
+package 圆圈中最后剩下的数字;
+
+public class FinalNumberTest {
+    public static void main(String[] args){
+        FinalNumber fn=new FinalNumber();
+        System.out.println(fn.getFinalNumber(5,3));
+        System.out.println(fn.getFinalNumberQuick(5,3));
+    }
+}
