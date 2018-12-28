928550923b5bcc6853de8a5d9111eac83492def4
ZFB <316128660@qq.com>
2018-12-28 21:31:16 +0800

    2018/12/27

--- src/股票的最大利润/MaxDiff.java
+++ src/股票的最大利润/MaxDiff.java
@@ -1,17 +1,17 @@
 package 股票的最大利润;
 
 public class MaxDiff {
-    public int getMaxDiff(int[] a){
-        if(a==null||a.length<2){
+    public int getMaxDiff(int[] array){
+        if(array==null||array.length<2){
             return 0;
         }
-        int min=a[0];
-        int maxDiff=a[1]-a[0];
-        for (int i=2;i<a.length;i++){
-            if(a[i-1]<min){
-                min=a[i-1];
+        int min=array[0];
+        int maxDiff=array[1]-array[0];
+        for (int i=2;i<array.length;i++){
+            if(array[i-1]<min){
+                min=array[i-1];
             }
-            int diff=a[i]-min;
+            int diff=array[i]-min;
             if(diff>maxDiff){
                 maxDiff=diff;
             }
