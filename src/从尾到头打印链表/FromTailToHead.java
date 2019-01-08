f48bd7152b1d17467441174579efa01b9e453455
ZFB <316128660@qq.com>
2019-01-08 17:07:48 +0800

    2018/1/8 四种解法详见https://blog.csdn.net/qq_38646470/article/details/78826223

--- src/从尾到头打印链表/FromTailToHead.java
+++ src/从尾到头打印链表/FromTailToHead.java
@@ -1,5 +1,28 @@
 package 从尾到头打印链表;
 
 public class FromTailToHead {
+    class Node{
+        int val;
+        Node next;
+    }
+    public void Reverse(Node head){
+        if(head==null)
+            return;
+        Node cur=head;
+        Node curNext=cur.next;
+        cur.next=null;
+        while (curNext!=null){
+            Node tmp=curNext.next;
+            curNext.next=cur;
+            cur=curNext;
+            curNext=tmp;
+        }
+        head=cur;
 
+        //打印链表
+        while (cur!=null){
+            System.out.println(cur.val);
+            cur=cur.next;
+        }
+    }
 }
