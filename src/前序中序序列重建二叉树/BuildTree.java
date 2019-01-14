c8e7b81ad38bdf3c6e8dd5972d341da08d3bb7cc
ZFB <316128660@qq.com>
2019-01-14 21:32:04 +0800

    2018/1/14 注意递归退出条件及遍历数组判断是否超出边界

--- src/前序中序序列重建二叉树/BuildTree.java
+++ src/前序中序序列重建二叉树/BuildTree.java
@@ -3,22 +3,27 @@ package 前序中序序列重建二叉树;
 import 树的遍历.BinNode;
 
 public class BuildTree {
-    public BinNode build(int[] pre,int[] mid){
-        return buildCore(pre,mid,0,pre.length-1,0,mid.length-1);
+    public BinNode build(int[] pre, int[] mid) {
+        if (pre == null || mid == null || pre.length == 0 || mid.length == 0 || pre.length != mid.length)
+            return new BinNode();
+        return buildCore(pre, mid, 0, pre.length - 1, 0, mid.length - 1);
     }
 
-    public BinNode buildCore(int[] pre,int[] mid,int prelo,int prehi,int midlo,int midhi){
-        if(pre==null||mid==null||pre.length==0||mid.length==0||pre.length!=mid.length)
-            return new BinNode();
-        int rootVal=pre[prelo];
-        BinNode root=new BinNode(rootVal);
-        int index=midlo;
-        while(mid[index]!=rootVal){
+    public BinNode buildCore(int[] pre, int[] mid, int prelo, int prehi, int midlo, int midhi) {
+
+        if (prelo > prehi || midlo > midhi) {          //没加递归退出条件
+            return null;
+        }
+        int rootVal = pre[prelo];
+        BinNode root = new BinNode(rootVal);
+        int index = midlo;
+        while (index < mid.length && mid[index] != rootVal) { //易忘判断index < mid.length
             index++;
         }
-        int subLength=index-midlo;
-        root.left=buildCore(pre,mid,prelo+1,prelo+subLength,index-subLength,index-1);
-        root.right=buildCore(pre,mid,prelo+subLength+1,prehi,index+1,midhi);
+        int subLength = index - midlo;
+        //root.left=buildCore(pre,mid,prelo+1,prelo+subLength,index-subLength,index-1);
+        root.left = buildCore(pre, mid, prelo + 1, prelo + subLength, midlo, index - 1);
+        root.right = buildCore(pre, mid, prelo + subLength + 1, prehi, index + 1, midhi);
         return root;
     }
 }
